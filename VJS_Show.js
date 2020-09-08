d8d30b98eba55422a9eed261d31a5dc8b4a343ac
Vantus555 <nindzay1998@gmail.com>
2020-09-08 19:07:57 +0300

    show. Быстрый вариант. Лево, вниз, лево-низ

--- VJS_Show.js
+++ VJS_Show.js
@@ -1,70 +1,82 @@
 //import VantusJS from VantusJS.js 
 
 let settingsShow = {
-    //func: 'linear',
-    property: ['height', 'width'],
-    move: 'down',
+    func: 'linear',
+    direction: 'down',
     display: 'block', 
     speed: 0
-}
-
-function show1(speed, h, node){
-    h.forEach(element => {
-        element.forEach((value, key, map) =>{
-            for (let j = 0; j <= parseInt(value); j += parseInt(value)/speed) {
-                setTimeout(function(){
-                    let myMap = new Map();
-                    myMap.set(key, j + 'px');
-                    V(node).css(myMap);
-                }, speed / parseInt(value) + speed / parseInt(value) * j);
-            }
-        });
-    });
 }
 
 VantusJS.prototype.show = function(settings)
 {
     let elems = this;
     let display = settings.display ? settings.display : settingsShow.display;
-    elems.css({'display' : display});
-    let h = elems.css(settingsShow.property);
-    console.log(h);
-
+    let direction = settings.direction ? settings.direction : settingsShow.direction;
     let speed = settings.speed ? settings.speed : settingsShow.speed;
-    if(speed != 0){
-        if(h instanceof Map){
-            let arr = elems.array();
-            for (let i = 0; i < arr.length; i++) {
-                const element = arr[i];
-                h.forEach((value, key, map) =>{
-                    for (let j = 0; j <= parseInt(value); j += parseInt(value)/speed) {
-                        setTimeout(function(){
-                            let myMap = new Map();
-                            myMap.set(key, j + 'px');
-                            V(element).css(myMap);
-                        }, speed / parseInt(value) + speed / parseInt(value) * j);
-                    }
-                });
-            }
-        }
-        else if(typeof(h) == 'string'){
-            for (let j = 0; j <= parseInt(h); j += parseInt(h)/speed) {
-                setTimeout(function(){
-                    let prop = settings.property ? settings.property : settingsShow.property;
-                    let mass = new Map;
-                    mass.set(prop, j + 'px');
-                    elems.css(mass);
-                }, speed / parseInt(h) + speed / parseInt(h) * j);
-            }
-        }
-        else{
-            let arr = elems.array();
-            for (let i = 0; i < arr.length; i++) {
-                const node = arr[i];
-                show1(speed, h, node);
-            }
-        }
+    let func = settings.func ? settings.func : settingsShow.func;
+
+    let stepOpacity = 1/(speed+1);
+
+    elems.css({'display' : display});
+
+    if(direction == 'down'){
+        elems.array().forEach(element => {
+            let opacity = stepOpacity;
+            let Velem = V(element);
+            Velem.css({'overflow' : 'hidden'});
+            let height = parseFloat(Velem.css('height'));
+            if(height!=0)
+                for (let i = 0; i <= height; i+=height/speed) {
+                    setTimeout(function(){
+                        Velem.css({
+                            'height': i + 'px',
+                            'opacity': opacity
+                        });
+                        opacity += stepOpacity;
+                    }, speed/height * i);
+                }
+        });
     }
-        
+    if(direction == 'left'){
+        elems.array().forEach(element => {
+            let opacity = stepOpacity;
+            let Velem = V(element);
+            Velem.css({'overflow' : 'hidden'});
+            let width = parseFloat(Velem.css('width'));
+            if(width!=0)
+                for (let i = 0; i <= height; i+=width/speed) {
+                    setTimeout(function(){
+                        Velem.css({
+                            'width': i + 'px',
+                            'opacity': opacity
+                        });
+                        opacity += stepOpacity;
+                    }, speed/width * i);
+                }
+        });
+    }
+    if(direction == 'left_down'){
+        elems.array().forEach(element => {
+            let opacity = stepOpacity;
+            let Velem = V(element);
+            Velem.css({'overflow' : 'hidden'});
+            let height = parseFloat(Velem.css('height'));
+            let width = parseFloat(Velem.css('width'));
+            let stepWidth = width/speed;
+            let countWidth = stepWidth;
+            if(height!=0 && height!=0)
+                for (let i = 0; i <= height; i+=height/speed) {
+                    setTimeout(function(){
+                        Velem.css({
+                            'height': i + 'px',
+                            'width': countWidth + 'px',
+                            'opacity': opacity
+                        });
+                        opacity += stepOpacity;
+                        countWidth += stepWidth;
+                    }, speed/height * i);
+                }
+        });
+    }
 
 }
